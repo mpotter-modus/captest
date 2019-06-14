69cca41cf49c6eec32014c62f47c036dd7f6a8e1
Mark Potter <mark.potter@moduscreate.com>
2019-06-14 16:16:43 -0400

    add some deps, use a Capacitor plugin

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <img :src="imgSrc" class="custom-img" />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { Plugins, CameraResultType } from '@capacitor/core';
+
+const { Camera } = Plugins;
 
 export default {
   name: 'app',
   components: {
     HelloWorld
-  }
+  },
+  data: () => ({
+    imgSrc: '',
+  }),
+  mounted() {
+    Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri
+    }).then((image) => {
+      this.imgSrc = image.webPath;
+    });
+  },
 }
 </script>
 
@@ -24,5 +40,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.custom-img {
+  width: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
+import { defineCustomElements } from '@ionic/pwa-elements/loader'
 import Ionic from '@ionic/vue'
 import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+defineCustomElements(window)
 Vue.use(Ionic)
 
 new Vue({
